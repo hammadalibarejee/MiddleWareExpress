f91d5729947a2dce7a688b01dd61d6085c2b9e09
Hammad Ali <34187587+hammadalibarejee@users.noreply.github.com>
2021-12-14 17:23:57 +0500

    Add files via upload

---
+++ server.mjs
@@ -0,0 +1,65 @@
+import express from "express";
+import fs from "fs";
+// import morgan from "morgan ";
+
+const PORT=3000 || process.env.PORT;
+const app=express();
+
+const user=fs.readFileSync('./data/sample.json','utf8');
+const hello = (req,res)=>{
+    console.log(req.requestTime);
+    res.status(200).json({
+        status:"Success",
+        Time: req.requestTime ,
+        Data:"Hello World"
+
+    })
+}
+const showUser= (req,res)=>{
+    res.status(200).json({
+        status:"Success",
+        Data: user
+    })
+}
+
+const postUser=(req,res)=>{
+    
+    const newUser= Object.assign(req.body);
+    // user.push(newUser);
+
+    fs.writeFile('./data/sample.json',JSON.stringify(user,err=>{
+        res.status(200).json({
+            status:"success",
+            data:{
+                user:newUser
+            }
+        })
+    }))
+
+
+}
+
+// app.use(morgan('dev'));
+app.use(express.json());
+
+app.use((req,res,next)=>{
+    console.log("Hello from the middle ware")
+    next();
+})
+
+app.use((req,res,next)=>{
+    req.requestTime = new Date().toISOString;
+    next();
+})
+app.route("/")
+    .get(hello);
+
+app.route("/users")
+    .get(showUser) 
+    .post(postUser);
+
+
+app.listen(PORT,()=>{
+    console.log(`The application is hosted on  http://localhost:${PORT}`);
+})
+
